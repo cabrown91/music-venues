bf6f6def9048586657d322e0a685677716e5c584
cabrown91 <cleo.aliciabrown@gmail.com>
2016-09-08 16:41:26 -0700

    added hard-coded data to app.js

---
+++ public/scripts/app.js
@@ -0,0 +1,45 @@
+var venuesList = [];
+venuesList.push({
+  name: "Billy Graham Civic Auditorium",
+  address: "99 Grove St, San Francisco, CA 94102",
+  website: "http://www.billgrahamcivicauditorium.com/",
+  image: "http://www.inetours.com/Pages/images/Civic-Center/Cvic-Auditorium_9865.jpg"
+});
+venuesList.push({
+  name: "Billy Graham Civic Auditorium",
+  address: "99 Grove St, San Francisco, CA 94102",
+  website: "http://www.billgrahamcivicauditorium.com/",
+  image: "http://www.inetours.com/Pages/images/Civic-Center/Cvic-Auditorium_9865.jpg"
+});
+venuesList.push({
+  name: "Billy Graham Civic Auditorium",
+  address: "99 Grove St, San Francisco, CA 94102",
+  website: "http://www.billgrahamcivicauditorium.com/",
+  image: "http://www.inetours.com/Pages/images/Civic-Center/Cvic-Auditorium_9865.jpg"
+});
+venuesList.push({
+  name: "Billy Graham Civic Auditorium",
+  address: "99 Grove St, San Francisco, CA 94102",
+  website: "http://www.billgrahamcivicauditorium.com/",
+  image: "http://www.inetours.com/Pages/images/Civic-Center/Cvic-Auditorium_9865.jpg"
+});
+venuesList.push({
+  name: "Billy Graham Civic Auditorium",
+  address: "99 Grove St, San Francisco, CA 94102",
+  website: "http://www.billgrahamcivicauditorium.com/",
+  image: "http://www.inetours.com/Pages/images/Civic-Center/Cvic-Auditorium_9865.jpg"
+});
+venuesList.push({
+  name: "Billy Graham Civic Auditorium",
+  address: "99 Grove St, San Francisco, CA 94102",
+  website: "http://www.billgrahamcivicauditorium.com/",
+  image: "http://www.inetours.com/Pages/images/Civic-Center/Cvic-Auditorium_9865.jpg"
+});
+
+$(document).ready(function(){
+  console.log('app.js loaded!');
+});
+
+function renderVenues(venue) {
+  console.log('rendering venue:', venue);
+}
