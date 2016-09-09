859103545d4682fc8024fbccd95cd2a1db02d4fc
Andrew Cordivari <andrewcord@William-Cordivaris-MacBook-Pro.local>
2016-09-09 12:00:12 -0700

    added checkboxes to html, started building routes

--- server.js
+++ server.js
@@ -1,11 +1,38 @@
 var express = require('express');
+  bodyParser = require('body-parser');
+  db = require('./models');
+
 
 var app = express();
-var bodyParser = require('body-parser');
 
+// serves static files in public directory
 app.use(express.static(__dirname + '/public'));
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', function homepage (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+//sets the root route
+app.get('/', function (req, res) {
+  res.sendFile('views/index.html' , { root : __dirname});
+});
+
+// get all the venues
+
+app.get('/api/venues', function (req, res) {
+  db.Book.find()
+  .populate('amenities')
+  .exec(function(err, venues) {
+    if(err) { return console.log("index error!"); }
+    res.json(venues);
+  });
+});
+
+//// creates new venue thru form
+app.post('api/venues', function(req, res) {
+  var newVenue = new db.Venue({
+    name: req.body.name,
+    address: req.body.address,
+    website: req.body.website,
+    image: req.body.image,
+
+  });
 });
