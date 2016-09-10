043d0273b39bad646e9c376dbbf1cef7172d2e51
cabrown91 <cleo.aliciabrown@gmail.com>
2016-09-10 08:43:06 -0700

    updated documentation json

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -4,7 +4,9 @@ function index(req, res) {
     documentation_url: "https://github.com/cabrown91/music-venues",
     base_url: "https://salty-bayou-63471.herokuapp.com/",
     endpoints: [
-      {method: "GET", path: "/api", description: "Describes available endpoints"}
+      {method: "GET", path: "/api", description: "Describes available endpoints"},
+      {method: "GET", path: "/api/venues", description: "Get all the venues in the database"},
+      {method: "POST", path: "/api/venues", description: "Create a new venue in the database"},
     ]
   });
 }
