e8421ac15a926a497d631bc835c98c6e20e89ab8
cabrown91 <cleo.aliciabrown@gmail.com>
2016-09-10 08:43:26 -0700

    removed extraneous code from server file

--- server.js
+++ server.js
@@ -18,52 +18,15 @@ app.get('/', function (req, res) {
 });
 
 var controllers = require('./controllers');
-// get all the venues
-
-
-var venuesList = [
-  {name: "Fox Theater",
-  address: "1807 Telegraph Ave, Oakland, CA 94612",
-  website: "http://www.apeconcerts.com/venues/fox-theater-oakland/",
-  image: "https://resize.rbl.ms/simage/https%3A%2F%2Fassets.rbl.ms%2F5501727%2F980x.jpg/2000,2000/jUdFKqL6Ake8Kqoy/img.jpg"
-},
-{
-  name: "Billy Graham Civic Auditorium",
-  address: "99 Grove St, San Francisco, CA 94102",
-  website: "http://www.billgrahamcivicauditorium.com/",
-  image: "http://www.inetours.com/Pages/images/Civic-Center/Cvic-Auditorium_9865.jpg"
-},
-{
-  name: "The Fillmore",
-  address: "1805 Geary Blvd, San Francisco, CA 94115",
-  website: "http://thefillmore.com/",
-  image: "http://www.sanfranciscodays.com/photos/large/the-fillmore.jpg"
-},
-{
-  name: "Great American Music Hall",
-  address: "859 O'Farrell St, San Francisco, CA 94109",
-  website: "http://www.slimspresents.com/",
-  image: "https://upload.wikimedia.org/wikipedia/commons/6/6a/Great_American_Music_Hall_(April_1976).jpg"
-},
-{
-  name: "The Independent",
-  address: "628 Divisadero St, San Francisco, CA 94117",
-  website: "http://www.theindependentsf.com/",
-  image: "http://ww2.kqed.org/bayareabites/wp-content/uploads/sites/24/2013/12/indysf-guide1000.jpg"
-},
-{
-  name: "Boom Boom Room",
-  address: "1601 Fillmore St, San Francisco, CA 94115",
-  website: "http://www.boomboomroom.com/tickets-schedule/",
-  image: "http://img1.sunset.timeinc.net/sites/default/files/image/city-guides/san-francisco/sf-attractions-boom-boom-room-0209-x.jpg"
-}
-];
-
 
 app.get('/api', controllers.api.index);
-
+// get all the venues
 app.get('/api/venues', controllers.venues.index);
-app.
+
+app.post('/api/venues',controllers.venues.create);
+
+// app.get('/api/venues/:venuesId', controllers.venues.show);
+// app.post('/api/amenitites', controllers.amenities.create);
 
 //// creates new venue thru form
 
