cc455de975e3d996c8428df0c68daa4f42b6cc25
cabrown91 <cleo.aliciabrown@gmail.com>
2016-09-09 18:15:51 -0700

    update api controller

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,10 +1,8 @@
-
-
 function index(req, res) {
   res.json({
     message: "Welome to our app! Locate cool music venues in San Francisco.",
     documentation_url: "https://github.com/cabrown91/music-venues",
-    // base_url: "http://tunely.herokuapp.com",
+    base_url: "https://salty-bayou-63471.herokuapp.com/",
     endpoints: [
       {method: "GET", path: "/api", description: "Describes available endpoints"}
     ]
