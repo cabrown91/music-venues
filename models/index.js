a3e4eb5c4de8a5e6bd69c457d09890ad3642af50
Andrew Cordivari <andrewcord@William-Cordivaris-MacBook-Pro.local>
2016-09-12 12:00:25 -0700

    prepared for heroku deployment 1

--- models/index.js
+++ models/index.js
@@ -1,5 +1,8 @@
 var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/music-venues");
+// mongoose.connect("mongodb://localhost/music-venues");
+mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/music-venues");
+  // URI = "mongodb://heroku_3gbs0pp8:22cbppsvpl3iq8j55stmdhpqvd@ds029466.mlab.com:29466/heroku_3gbs0pp8" );
+
 
 
 var Venue = require('./venues');
